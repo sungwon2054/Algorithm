8c42c1642281a4a829de173b84189445fa382bf3
USER <user@AL01443210.local>
2021-09-18 14:29:07 +0900

    Boj

--- Online judge/Boj/Python/1431.py
+++ Online judge/Boj/Python/1431.py
@@ -1,6 +1,13 @@
 a = []
+b = {}
 for _ in range(int(input())):
   a.append(input())
-a.sort()
-print(a)
-
+for x in a:
+  sum = 0
+  for y in x:
+    if y.isdigit():
+      sum += int(y)
+  b[x] = sum
+b = sorted(b.items(), key=lambda x: (len(x[0]), x[1], x[0]) )
+for x in b:
+  print(x[0])
